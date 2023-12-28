73da067b529061880384edf262dfbce361b75ef1
Poonam-gaian <poonam.r@gaiansolutions.com>
2023-12-28 09:51:05 +0530

    branch data fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,21 @@ import { GiteaService } from './gitea.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NewGitea_POC';
+  title = 'gitea';
   repoName: string = '';
   filePath: string = '';
+  fileContent: string = '';
   createdRepoMessage: string = '';
+  fetchedFileContent: string = '';
+  username:string = '';
+  allRepos:any = '';
+  allFiles: any = '';
+  currentPath: string = '';
+  updateFile: any;
+  fileName: any;
+  updateFileName: string = '';
+  allBranches: any;
+  branch: any;
 
 
 
@@ -26,4 +37,133 @@ export class AppComponent {
       }
     });
   }
+
+  fetchAllRepos(){
+    this.giteaService.getAllRepos(this.username).subscribe({
+      next:(response)=>{
+        this.allRepos = response;
+        console.log(response)
+      },
+      error:(error)=>{
+        console.error(error);
+      }
+    })
+  }
+  fetchAllFiles(){
+    this.giteaService.getAllFiles(this.username, this.repoName, this.filePath).subscribe({
+      next:(response)=>{
+        this.allFiles = response;
+        console.log(response)
+      },
+      error:(error)=>{
+        console.error(error);
+      }
+    })
+  }
+
+  // fetchBranch(){
+  //   this.giteaService.getBranch(this.username, this.repoName, this.branch).subscribe({
+  //     next:(response)=> {
+  //       this.allBranches = response;
+  //       console.log(response);
+  //   },
+  //   error:(err)=>{
+  //     alert("No Branch Found")  
+  //     }
+  // })}
+  loading: boolean = false;
+
+  fetchBranch(): void {
+    this.giteaService.getBranch(this.username, this.repoName).subscribe({
+      next: (branches) => {
+        this.allBranches = branches;
+        console.log('Branches:', branches);
+      },
+      error: (error) => {
+        console.error('Error fetching branches:', error);
+      }
+ 
+  })
+}
+  // fetchBranchDetails(branchName: string): void {
+  //   // if (!this.branch) {
+  //   //   console.error('Branch name is undefined.');
+  //   //   return;
+  //   // }
+  //   this.giteaService.getBranchDetails(this.username, this.repoName, branchName).subscribe({
+  //     next: (branchDetails: any) => {
+  //       console.log('Branch details:', branchDetails);
+  //     },
+  //     error: (error: any) => {
+  //       console.error(`Error fetching details for branch '${branchName}':`, error);
+  //     }
+  //   });
+  // }
+
+  DiveIn(file: any): void {
+    if (file.type === 'file') {
+
+      this.giteaService.getFileContent(this.username, this.repoName, file.path).subscribe({
+        next: (response) => {
+          this.fileContent = atob(response.content); // Decode 
+        },
+        error: (error) => {
+          console.error('Error fetching file content:', error);
+        }
+      });
+    } else if (file.type === 'dir') {
+      this.giteaService.getAllFiles(this.username, this.repoName, file.path).subscribe({
+        next: (response) => {
+          this.allFiles = response;
+          console.log(response);
+        },
+        error: (error) => {
+          console.error("Error fetching files for this folder");
+        }
+      });
+    }
+  }
+
+updateFiles(): void {
+  const filePath = this.fileName;
+  const fullPath = filePath ? `/${filePath}` : '';
+
+  this.giteaService.getFileContent(this.username, this.repoName, fullPath).subscribe({
+    next: (response) => {
+      const sha = response.sha;
+      const content = btoa(this.fileContent); // Encode content to Base64
+
+      if (sha) {
+        this.giteaService.updateFiles(this.username, this.repoName, fullPath, content, sha)
+          .subscribe({
+            next: (updateResponse) => {
+              this.updateFileName = updateResponse.name;
+              console.log(`File updated successfully: ${this.updateFileName}`);
+            },
+            error: (updateError) => {
+              console.error('Error updating file:', updateError);
+            }
+          });
+      } else {
+        // Create a new file
+        this.giteaService.createFile(this.username, this.repoName, fullPath, content)
+          .subscribe({
+            next: (createResponse) => {
+              this.updateFileName = createResponse.name;
+              console.log(`File created successfully: ${this.updateFileName}`);
+            },
+            error: (createError) => {
+              console.error('Error creating file:', createError);
+            }
+          });
+      }
+    },
+    error: (error) => {
+      console.error('Error fetching file content:', error);
+    }
+  });
+}
+
+
+
 }
--- src/app/gitea.service.ts
+++ src/app/gitea.service.ts
@@ -6,10 +6,14 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
   })
   export class GiteaService {
+
+
 //   private apiUrl = 'http://localhost:3000/api/v1';
 private apiUrl ='http://localhost:3000/api/v1';
   private username = 'poonamgaian';
-  private password = 'poonamgaian';
+  private password = '96659cbb8e0f7ecce6f67c843c9304427dc9af05';
+  getBranchDetails: any;
+
 
   constructor(private http: HttpClient) {}
 
@@ -31,5 +35,53 @@ private apiUrl ='http://localhost:3000/api/v1';
   
     return this.http.post(url, payload, { headers: this.getHeaders() });
   }
+
+  getAllRepos(username:string){
+    const url = `${this.apiUrl}/users/${username}/repos`
+     return this.http.get(url, { headers: this.getHeaders() });
+  }
+
+  getAllFiles(username: string, repo: string, path: string = ''): Observable<any> {
+    const fullPath = path ? `/${path}` : '';
+    const url = `${this.apiUrl}/repos/${username}/${repo}/contents${fullPath}`;
+    return this.http.get(url, { headers: this.getHeaders() });
+  }
+
+  getFileContent(username: string, repo: string, path: string = ''): Observable<any> {
+    const fullPath = path ? `/${path}` : '';
+    const url = `${this.apiUrl}/repos/${username}/${repo}/contents${fullPath}`;
+    return this.http.get(url, { headers: this.getHeaders() });
+  }
+
+
+  updateFiles(username: string, repo: string, path: string = '', content: string = '', sha: string): Observable<any> {
+    const fullPath = path ? `/${path}` : '';
+    const url = `${this.apiUrl}/repos/${username}/${repo}/contents${fullPath}`;
+    
+    const requestBody = {
+      message: 'Update file',
+      content: content,
+      sha: sha
+    };
+    return this.http.put(url, requestBody, { headers: this.getHeaders() });
+  }
   
+
+  createFile(username: string, repo: string, path: string, content: string): Observable<any> {
+    const fullPath = path ? `/${path}` : '';
+    const url = `${this.apiUrl}/${username}/${repo}/contents${fullPath}`;
+
+    const requestBody = {
+      message: 'Create file',
+      content: content,
+    };
+
+    return this.http.post(url, requestBody, { headers: this.getHeaders() });
+  }
+  
+
+  getBranch(owner: string, repo: string): Observable<any> {
+    const url = `${this.apiUrl}/repos/${owner}/${repo}/branches`;
+    return this.http.get<any>(url);
+  }
 }
